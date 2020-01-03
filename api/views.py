a0c7b760f8c96eaf1acdda94b338ae0b3682b777
HironoriSato <maza.hs5360@gmail.com>
2020-01-04 01:05:47 +0900

    [api]add:viewsにpermissino_classesを追加

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+
 from sample.models import MainList, SubList
 from .serializers import MainListSerializer, SubListSerializer
 from rest_framework import viewsets
@@ -7,8 +9,10 @@ from rest_framework import viewsets
 class MainListViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = MainList.objects.all()
     serializer_class = MainListSerializer
+    permission_classes = [IsAuthenticated]  # 認証済みのみアクセス可能
 
 
 class SubListViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = SubList.objects.all()
     serializer_class = SubListSerializer
+    permission_classes = [IsAuthenticated]  # 認証済みのみアクセス可能
