c0b4c2d99e2ebb36782c8c44f98029d20a797e5b
HironoriSato <maza.hs5360@gmail.com>
2020-01-04 00:55:03 +0900

    [config]add:認証用のルーティングを設定

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('api/auth/', include('djoser.urls.jwt')),  # 認証用URL(djoserがエンドポイントを用意)
 ]
